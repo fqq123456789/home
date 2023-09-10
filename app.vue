d889b31415059aac692ee6a132071a95f2c4c4bf
Guany <1084072690@qq.com>
2023-09-10 20:13:38 +0800

    chore: update

--- app.vue
+++ app.vue
@@ -9,3 +9,21 @@ useHead({
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<style>
+html,
+body,
+#__nuxt {
+  height: 100%;
+}
+
+body {
+  background-color: #ecf0f1;
+  color: #121212;
+}
+
+.dark body {
+  background-color: #121212;
+  color: #ecf0f1;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ['@unocss/reset/tailwind.css', '@/assets/styles/main.css'],
+  css: [
+    '@unocss/reset/tailwind.css',
+  ],
   devtools: {
     enabled: true,
   },
-  modules: ['@nuxtjs/color-mode', '@unocss/nuxt'],
+  modules: [
+    '@nuxtjs/color-mode',
+    '@unocss/nuxt',
+  ],
   colorMode: {
     classSuffix: '',
   },
